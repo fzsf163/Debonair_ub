0f9eea4c2d352a9197c147059ac9ba110cc2fd8d
fzs163 <yofaisol619@gmail.com>
2023-06-14 07:17:57 +0600

    modal added

--- src/components/AdminUsers.tsx
+++ src/components/AdminUsers.tsx
@@ -1,14 +1,15 @@
-
 import QueryUsers from "../query/QueryUsers";
 // import DataTable from "./Table";
 import AdminTable from "./AdminTable";
 import { Box } from '@mui/material';
 
+import FormModal from "./FormModal";
+
 export default function AdminUsers () {
 
     const { data, isLoading } = QueryUsers();
 
-    const userCount: number = data?.readEmployeeData?.length;
+    // const userCount: number = data?.readEmployeeData?.length;
 
     const userData = data?.readEmployeeData;
 
@@ -19,8 +20,13 @@ export default function AdminUsers () {
     return (
 
         <div>
-            <h1>Users: { userCount } </h1>
-            {/* <h1>admin user : { onlyAdmin?.length }</h1> */ }
+            <Box sx={ { m: 3, p: 2, display: "flex", alignItems: "center", justifyContent: "flex-start", gap: 4 } }>
+                <h2>Admin Users: { onlyAdmin?.length } </h2>
+                <div>
+                    |
+                </div>
+                <FormModal></FormModal>
+            </Box>
 
             {
                 !isLoading ?
--- src/components/EmployeeUsers.tsx
+++ src/components/EmployeeUsers.tsx
@@ -2,12 +2,14 @@ import { Box } from '@mui/material';
 import QueryUsers from '../query/QueryUsers';
 import EmployeeTable from './EmployeeTable';
 
+import FormModal from './FormModal';
+
 
 export default function EmployeeUsers () {
 
     const { data, isLoading } = QueryUsers();
 
-    const userCount: number = data?.readEmployeeData?.length;
+    // const userCount: number = data?.readEmployeeData?.length;
 
     const userData = data?.readEmployeeData;
 
@@ -17,8 +19,15 @@ export default function EmployeeUsers () {
     return (
 
         <div>
-            <Box flexDirection={ 'column' }>
-                <h1>Users: { userCount } </h1>
+            <Box sx={ { m: 3, p: 2, display: "flex", alignItems: "center", justifyContent: "flex-start", gap: 4 } }>
+                <h2>Employee Users: { onlyEmployee?.length }  </h2>
+                <div>
+                    |
+                </div>
+                <FormModal></FormModal>
+            </Box>
+            <Box>
+
                 {
                     !isLoading ?
                         <Box sx={ { m: 3, p: 2, border: 1, borderRadius: 2 } }>
